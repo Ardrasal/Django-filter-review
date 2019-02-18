4d2cc5e901dd5ff92b1e4eb023d4d5bd3d3fab69
Ardra Salemy <ardrasalemy@gmail.com>
2019-02-18 13:58:23 -0500

    Changes search to icontains. My project was missing his cached css file, but I was able to see how it would work following his video.

--- djangofilter/snippets/filters.py
+++ djangofilter/snippets/filters.py
@@ -4,6 +4,23 @@ from .models import Snippet
 
 class SnippetFilter(django_filters.FilterSet):
 
+    CHOICES = (
+        ('ascending', 'Ascending'),
+        ('descending', 'Descending')
+    )
+
+    ordering = django_filters.ChoiceFilter(label='Ordering', choices=CHOICES, method='filter_by_order')
+
     class Meta:
         model = Snippet
-        fields = ('title', 'body')
+        # the following attribute only works for exact full search
+        # fields = ('title', 'body')
+        # In order to capture results that just contain the search parameter, set fields to a dictionary and use 'icontains'
+        fields = {
+            'title': ['icontains'],
+            'body': ['icontains'],
+        }
+
+    def filter_by_order(self, queryset, name, value):
+        expression = 'created' if value == 'ascending' else '-created'
+        return queryset.order_by(expression)
